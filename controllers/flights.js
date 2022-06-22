833ea39339bb732032af580b8d732e603a4d9eb4
Pierce Petit <ppetit2496@gmail.com>
2022-06-22 08:30:01 -0500

    work show.ejs commit

--- controllers/flights.js
+++ controllers/flights.js
@@ -7,14 +7,16 @@ function newFlight(req, res) {
     })
 }
 function create(req, res) {
-    console.log(req.body);
-    const flight = new Flight(req.body);
-    flight.save(function(err) {
-        if (err) return res.render("flights/new");
-        console.log(flight);
-        res.redirect("flights/");
-    });
-  
+    console.log(req.body)
+    Flight.create(req.body)
+    .then(flight => {
+        console.log("created flight:", flight)
+        res.redirect("/flights");
+    })
+    .catch(err => {
+        console.log(err)
+        res.redirect('/flights')
+    })
 }
 
 function index(req, res) {
@@ -22,16 +24,35 @@ function index(req, res) {
     .then(flights => {
         res.render('flights/index',{
             flights: flights,
-            title: 'All Movies',
+            title: 'All flights',
         })
     })
 }
 
 function show(req, res) {
-    flight.findById(req.params.id)
-    .then(fight => {
-        res.render('flights/show')
-        
+    Flight.findById(req.params.id)
+    .then(flights => {
+        console.log(flights)
+        res.render('flights/show',{
+            flights: flights,
+            title: 'Flights Detail'
+        })
+    })
+    .catch(err => {
+        console.log(err)
+        res.redirect('/')
+    })
+}
+
+function deleteFlight(req, res) {
+    console.log('deter')
+    Flight.findByIdAndDelete(req.params.id)
+    .then(() => {
+        res.render('/flights')
+    })
+    .catch(err => {
+        console.log(err)
+        res.redirect('/')
     })
 }
 
@@ -40,4 +61,6 @@ export {
     newFlight as new,
     create,
     index,
+    show,
+    deleteFlight as delete,
 }
